af5030de3d718971de032ce94e524f8b9553357d
maccwinter <matthew.winter1@louisiana.edu>
2020-10-08 17:53:32 -0600

    Finally!

--- practice.R
+++ practice.R
@@ -1,12 +1,15 @@
 library(ape)
 packageVersion("ape")
 
+
+
 hex <- read.table("GenbankHexa.csv", quote="\"", stringsAsFactors=FALSE)
 oct <- read.table("genbankOcto.csv", quote ="\"", stringsAsFactors =FALSE)
+head(hex)
 
-fd 
 as.list(hex)$V1 -> hexL
 as.list(oct)$V1 -> octL
+head(hexL)
 
 hexgen <- read.GenBank(hexL, species.names = T)
 octgen <- read.GenBank(octL, species.names = T)
