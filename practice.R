419b55a4b712d7388c2fc05a565ed7e9dc8f547e
maccwinter <matthew.winter1@louisiana.edu>
2020-10-08 16:39:42 -0600

    coded to download genbank data

--- practice.R
+++ practice.R
@@ -5,3 +5,9 @@ hex <- read.table("GenbankHexa.csv", quote="\"", stringsAsFactors=FALSE)
 oct <- read.table("genbankOcto.csv", quote ="\"", stringsAsFactors =FALSE)
 
 fd 
+as.list(hex)$V1 -> hexL
+as.list(oct)$V1 -> octL
+
+hexgen <- read.GenBank(hexL, species.names = T)
+octgen <- read.GenBank(octL, species.names = T)
+
