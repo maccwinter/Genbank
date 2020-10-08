960b4fa6244669672a91d594a822ed212e1a9c3e
maccwinter <matthew.winter1@louisiana.edu>
2020-10-08 16:25:22 -0600

    named and downloaded datasets

---
+++ practice.R
@@ -0,0 +1,7 @@
+library(ape)
+packageVersion("ape")
+
+hex <- read.table("GenbankHexa.csv", quote="\"", stringsAsFactors=FALSE)
+oct <- read.table("genbankOcto.csv", quote ="\"", stringsAsFactors =FALSE)
+
+fd 
