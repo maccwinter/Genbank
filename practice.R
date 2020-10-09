4603db6dd46194ca243a6e029dfb29b932dfa214
maccwinter <matthew.winter1@louisiana.edu>
2020-10-08 18:43:10 -0600

    I created a data fram with the species name and the accession number for hexacorallia and octocorallia

--- practice.R
+++ practice.R
@@ -5,11 +5,11 @@ packageVersion("ape")
 
 hex <- read.table("GenbankHexa.csv", quote="\"", stringsAsFactors=FALSE)
 oct <- read.table("genbankOcto.csv", quote ="\"", stringsAsFactors =FALSE)
-head(hex)
+
 
 as.list(hex)$V1 -> hexL
 as.list(oct)$V1 -> octL
-head(hexL)
+
 
 hexgen <- read.GenBank(hexL, species.names = T)
 octgen <- read.GenBank(octL, species.names = T)
